47ed56f3fa05f5aa05a28e95d10b4a00814c56b2
kev1931 <kevin.rossi1931@gmail.com>
2023-06-01 16:39:57 +0200

    modificato il routing

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -3,28 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { TemplateComponent } from './template.component';
 
 const routes: Routes = [
-{ 
+{
     path: "", component: TemplateComponent,
-  children: 
+  children:
   [
     {
       path: "incarichi",
       loadChildren: () => import("./../pages/incarichi/incarichi-view/incarichi-view.module").then(m => m.IncarichiViewModule)
-    },
-    {
-      path: "allegati",
-      loadChildren: () => import("./../pages/allegati/allegati-view/allegati-view.module").then(m => m.AllegatiViewModule)
-    },
-    {
-      path: "dati",
-      loadChildren: () => import("./../pages/dati/dati-view/dati-view.module").then(m => m.DatiViewModule)
     },
 
   ]
 
 
 }
-  
+
 ];
 
 @NgModule({
